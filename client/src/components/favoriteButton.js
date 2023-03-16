07af1095fe6f3ce9d15a6dc6c1d1639707193aef
Anneice Manzanares <mountains.stars@gmail.com>
2023-03-16 15:50:48 -0600

    update to favorites button - not working

--- client/src/components/favoriteButton.js
+++ client/src/components/favoriteButton.js
@@ -1,19 +1,10 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import { faHeart as faHeartEmpty } from '@fortawesome/free-regular-svg-icons'
 
 const FavoriteButton = (props) => {
 
-    // const [isFavorite, setIsFavorite] = useState(false)
-    // const onChange= (e) => {
-    //     e.preventDefault()
-    //     if (isFavorite===true) {
-    //         setIsFavorite(false)
-    //     } else {
-    //         setIsFavorite(true)
-    //     }
-    // }
     const onClick= async (e) => {
         e.preventDefault()
         try {
@@ -24,6 +15,7 @@ const FavoriteButton = (props) => {
                     "Content-Type": "application/json"
                 }
             })
+
             } else {
                             await fetch(`http://localhost:8080/api/events/${props.id}/favorite`, {
                 method: "DELETE",
@@ -31,15 +23,32 @@ const FavoriteButton = (props) => {
                     "Content-Type": "application/json"
                 }
             })
+
             }
         } catch (error) {
             
         }
     }
 
+    const [isFavorite, setIsFavorite] = useState(false);
+    const [isLoading, setIsLoading] = useState(false)
+    const handleOnClick= useCallback(() => {
+        setIsLoading(true);
+
+        onClick()
+        .then (() => {
+            setIsFavorite((current) => !current);
+            setIsLoading(false);
+        })
+        .catch (() => {
+            setIsLoading(false)
+        })
+    }, [])
+
     return (
-        <div onClick={onClick}>
-        {props.isfavorite ? <FontAwesomeIcon icon={faHeart} /> : <FontAwesomeIcon icon={faHeartEmpty} /> }
+        <div onClick={handleOnClick}
+        disabled={isLoading}>
+        {isLoading ? "Loading..." :isFavorite? <FontAwesomeIcon icon={faHeart} /> : <FontAwesomeIcon icon={faHeartEmpty} /> }
         
         </div>
     )
